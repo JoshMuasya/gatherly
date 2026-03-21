80d3bde30cfe59dfb437edb24a29918776b3ce9c
joshua <joshua.m@offtech.ca>
2026-03-21 11:07:07 +0300

    Made some changes

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -71,7 +71,7 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const snapshot = await adminDb
       .collection("events")
--- src/app/api/tickets/[registrationId]/route.ts
+++ src/app/api/tickets/[registrationId]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { adminDb } from "@/lib/firebase/firebase-admin";
 
-export async function GET(request: NextRequest, { params }: { params: { registrationId: string } }) {
+export async function GET( { params }: { params: { registrationId: string } }) {
   const { registrationId } = params;
 
   if (!registrationId) {
